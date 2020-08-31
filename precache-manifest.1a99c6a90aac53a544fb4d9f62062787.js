63cb093dd0792f62436dff459647d91757db1ee3
Anna Arkhipova <qywy1234@gmail.com>
2020-08-31 22:21:02 +1000

    Updates

---
+++ precache-manifest.1a99c6a90aac53a544fb4d9f62062787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70de4440e2c7513c70f6afb5f6f0508e",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "8ef47c68eb0976584a7b",
+    "url": "/movies-app/static/css/main.3b6309d2.chunk.css"
+  },
+  {
+    "revision": "ae402b736bc4b0415c60",
+    "url": "/movies-app/static/js/2.b392776b.chunk.js"
+  },
+  {
+    "revision": "a56aa9951b0e669f6f38adb273f204e1",
+    "url": "/movies-app/static/js/2.b392776b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef47c68eb0976584a7b",
+    "url": "/movies-app/static/js/main.1be146e3.chunk.js"
+  },
+  {
+    "revision": "6f4ce7a2b201b6f9f6f8",
+    "url": "/movies-app/static/js/runtime-main.94bcfe81.js"
+  }
+]);
